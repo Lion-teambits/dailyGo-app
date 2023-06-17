127755a7f2373414118f62c8f5e7b0235940368d
Rena Sakata <rena.sakata.cn@gmail.com>
2023-06-17 00:14:55 -0700

    Testing

--- src/screens/tabnav/HomeScreen.js
+++ src/screens/tabnav/HomeScreen.js
@@ -1,90 +1,37 @@
 import React, { useEffect, useState } from 'react';
 import { View, Text, Platform } from 'react-native';
-3;
-import {
-  requestAuthorization,
-  getStepCount,
-  getDistance,
-  getCalories
-} from '../../api/healthInfoAPI';
+import { fetchActivityData } from '../../api/healthInfoAPI';
 
 const HomeScreen = () => {
-  if (Platform.OS === 'ios') {
-    const [todayStep, setTodayStep] = useState({});
-    const [todayDistance, setTodayDistance] = useState({});
-    const [todayEnergyBarned, setTodayEnergyBarned] = useState({});
 
-    useEffect(() => {
-      requestAuthorization((error) => {
-        if (error) {
-          console.log('[ERROR] Cannot grant permissions!');
-          return;
-        }
+  const [os, setOs] = useState('');
 
-        getStepCount((err, result) => {
-          if (err) {
-            console.error(err);
-            return;
-          }
-          setTodayStep(result);
-        });
-
-        getDistance((err, result) => {
-          if (err) {
-            console.error(err);
-            return;
-          }
-          setTodayDistance(result);
-        });
-
-        getCalories((err, result) => {
-          if (err) {
-            console.error(err);
-            return;
-          }
-          setTodayEnergyBarned(result);
-        });
-      });
-    }, []);
-
-    return (
-      <View>
-        <Text>This is iOS</Text>
-        <Text>Steps: {JSON.stringify(todayStep)}</Text>
-        <Text>Distance: {JSON.stringify(todayDistance)}</Text>
-        <Text>Calories: {JSON.stringify(todayEnergyBarned)}</Text>
-      </View>
-    );
-  } else if (Platform.OS === 'android') {
-    const [todayStep, setTodayStep] = useState({});
-    const [todayDistance, setTodayDistance] = useState({});
-    const [todayEnergyBarned, setTodayEnergyBarned] = useState({});
-
-    useEffect(() => {
-      async function retrieveData() {
-        try {
-          const stepResult = await getStepCount();
-          setTodayStep(stepResult);
-          const distanceResult = await getDistance();
-          setTodayDistance(distanceResult);
-          const caloriesResult = await getCalories();
-          setTodayEnergyBarned(caloriesResult);
-        } catch (error) {
-          console.log('Error :', error.message);
-        }
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await fetchActivityData();
+        console.log(data);
+      } catch (error) {
+        console.error(error);
       }
-      retrieveData();
-    }, []);
+    };
 
-    return (
-      <View>
-        <Text>This is android</Text>
-        <Text>Steps: {JSON.stringify(todayStep)}</Text>
-        <Text>Distance: {JSON.stringify(todayDistance)}</Text>
-        <Text>Calories: {JSON.stringify(todayEnergyBarned)}</Text>
-      </View>
-    );
-  }
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    if (Platform.OS === 'ios') {
+      setOs('iOS');
+    } else {
+      setOs('Android');
+    }
+  }, []);
+
+  return (
+    <View>
+      <Text>This is {os}</Text>
+    </View>
+  );
 };
 
 export default HomeScreen;
