590f3a8d1d92b1a77e2908433753e23ae4e91910
Rena Sakata <rena.sakata.cn@gmail.com>
2023-06-18 00:36:15 -0700

    Set background fetch feature

--- src/screens/tabnav/HomeScreen.js
+++ src/screens/tabnav/HomeScreen.js
@@ -2,10 +2,13 @@ import React, { useEffect, useState } from 'react';
 import { View, Text, Platform } from 'react-native';
 import { fetchActivityData } from '../../api/healthInfoAPI';
 
+// get Data when user open app or Home screen
+
+// setTimer to get data in foregraound
+
 const HomeScreen = () => {
 
   const [os, setOs] = useState('');
-
 
   useEffect(() => {
     if (Platform.OS === 'ios') {
