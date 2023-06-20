b7884afd25d0bc35c775cad0d268d08b03f16dd3
Rena Sakata <rena.sakata.cn@gmail.com>
2023-06-19 17:32:45 -0700

    Teest code

--- src/screens/tabnav/HomeScreen.js
+++ src/screens/tabnav/HomeScreen.js
@@ -1,15 +1,25 @@
 import React, { useEffect, useState } from 'react';
 import { View, Text, Platform } from 'react-native';
+import updateDatabase from '../../services/updateDatabase';
+import { retrieveChallenges } from '../../api/challengeService';
 
 // get Data when user open app or Home screen
 
 // setTimer to get data in foregraound
 
 const HomeScreen = () => {
-
   const [os, setOs] = useState('');
+  const [data, setdata] = useState();
 
   useEffect(() => {
+    const fetchData = async () => {
+      const userInfo = await updateDatabase(); // user_id
+      const challenge_data = await retrieveChallenges(userInfo);
+      setdata(challenge_data);
+      console.log(challenge_data);
+    };
+
+    fetchData();
     if (Platform.OS === 'ios') {
       setOs('iOS');
     } else {
@@ -20,6 +30,7 @@ const HomeScreen = () => {
   return (
     <View>
       <Text>This is {os}</Text>
+      <Text>{data ? JSON.stringify(data) : 'none'}</Text>
     </View>
   );
 };
