f1572f9204482f19a6a9a9291c9b285100815df4
Jungho Lee <nightfallb612@gmail.com>
2023-06-21 14:17:56 -0700

    Add challengeData

---
+++ src/data/challengeData.js
@@ -0,0 +1,25 @@
+export const challenges = [
+  {
+    id: 1,
+    title: "Summer Jam",
+    image: require("../../assets/images/summer_2023.jpg"),
+    description: "Summer time, yummy fireflese",
+    targetSteps: 30000,
+    currentSteps: 15000,
+    badgeImage: require("../../assets/images/badge_sample.jpg"),
+    badgeTitle: "Greate walker",
+    status: "active",
+  },
+  {
+    id: 1,
+    title: "Christmas event",
+    image: require("../../assets/images/challenge/christmas_2023.jpg"),
+    description: "Christmas time, yummy fireflese",
+    targetSteps: 20000,
+    currentSteps: 0,
+    badgeImage: require("../../assets/images/badge_sample.jpg"),
+    badgeTitle: "Greate walker",
+    status: "upcomming",
+  },
+  // Add more challenges...
+];
