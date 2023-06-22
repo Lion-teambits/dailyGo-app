f60f1c56cb968385e429bce3ba912190bc743205
Rena Sakata <rena.sakata.cn@gmail.com>
2023-06-22 14:52:20 -0700

    add variables

--- src/api/constants.js
+++ src/api/constants.js
@@ -1,9 +1,6 @@
-import {
-  BACKEND_SERVER_URL as envBackendURL,
-  TEST_UID as test_uid,
-} from "@env";
+import { BACKEND_SERVER_URL, TEST_UID as test_uid } from "@env";
 
-export const BACKEND_URL = envBackendURL;
+export const BACKEND_URL = BACKEND_SERVER_URL;
 
 // For testing, will be removed
 export const TEST_UID = test_uid;
