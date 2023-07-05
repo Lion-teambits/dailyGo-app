623d972aa4b32ac06377ea6de55a6e1bf2fcc0f6
Rena Sakata <rena.sakata.cn@gmail.com>
2023-07-05 16:08:36 -0700

    Update function to handle reward

--- src/services/receiveReward.js
+++ src/services/receiveReward.js
@@ -1,12 +1,17 @@
+import { retrieveUserInfo, updateUserInfo } from "../api/userService";
+import { calculateStreakDaysAndReward } from "./checkChallengeProgress";
 
 const receiveReward = async (user_id) => {
+  const userInfo = await retrieveUserInfo(user_id);
   const rewardObj = await calculateStreakDaysAndReward(user_id);
   await updateUserInfo(userInfo.uid, {
     fireflies: userInfo.fireflies + rewardObj.firefliesToday,
     hearts: userInfo.hearts + rewardObj.heartToday,
     streak_days: userInfo.streak_days + 1,
+    daily_goal_status: 3
+    // daily_goal_status: "competed" // check data type
   });
-  console.log("Reward received into user collection")
+  console.log("Reward received into user collection");
 };
 
 export default receiveReward;
