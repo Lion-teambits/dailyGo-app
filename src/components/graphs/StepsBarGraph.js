8d535d2839282abc5a6546965faf004d36b30fc7
Jungho Lee <nightfallb612@gmail.com>
2023-07-05 11:54:07 -0700

    Add color to Challenge list

--- src/components/graphs/StepsBarGraph.js
+++ src/components/graphs/StepsBarGraph.js
@@ -1,4 +1,5 @@
 import { Box, Text } from "native-base";
+import { ACCENT_MEDIUM, BG_MEDIUM } from "../../constants/colorCodes";
 
 const StepsBarGraph = ({ currentSteps, targetSteps, progressBarWidth }) => {
   return (
@@ -6,9 +7,9 @@ const StepsBarGraph = ({ currentSteps, targetSteps, progressBarWidth }) => {
       <Text>
         {currentSteps}/{targetSteps} steps
       </Text>
-      <Box bg="gray.300" height={6} borderRadius="full" overflow="hidden">
+      <Box bg={BG_MEDIUM} height={6} borderRadius="full" overflow="hidden">
         <Box
-          bg="yellow.400"
+          bg={ACCENT_MEDIUM}
           width={progressBarWidth}
           height={6}
           borderRadius="full"
--- src/constants/colorCodes.js
+++ src/constants/colorCodes.js
@@ -1,4 +1,19 @@
+export const PRIMARY_LIGHT = "#C1C8FF";
+export const PRIMARY_MEDIUM = "#4A5AE9";
 export const PRIMARY_DARK = "#000F92";
 
+export const SECONDARY_LIGHT = "#FFD4BF";
 export const SECONDARY_MEDIUM = "#FF5500";
+export const SECONDARY_DARK = "#9F3500";
+
+export const ACCENT_LIGHT = "#FDFF9C";
+export const ACCENT_MEDIUM = "#FFE500";
+export const ACCENT_DARK = "#A79600";
+
+export const BG_LIGHT = "#FFFFFF";
+export const BG_MEDIUM = "#EFEFEF";
+export const BG_DARK = "#38363D";
+
 export const DISABLED = "rgba(0, 0, 0, 0.15)";
+export const ERROR = "#D00000";
+export const SUCCESS = "#00D06C";
