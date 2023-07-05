19164fdadd722a3613ba1005988e60f6e3c3bdee
Rena Sakata <rena.sakata.cn@gmail.com>
2023-07-05 16:03:56 -0700

    Add modals

--- src/components/list/OngoingChallengeCard.js
+++ src/components/list/OngoingChallengeCard.js
@@ -1,62 +1,235 @@
-import { Progress, Center, Box } from "native-base";
+import {
+  Progress,
+  Center,
+  Box,
+  HStack,
+  VStack,
+  Image,
+  Divider,
+  Button,
+  Text,
+  View,
+} from "native-base";
 import { useEffect, useState } from "react";
-import { View, StyleSheet, Text, Image } from "react-native";
-import image1 from "../../../assets/images/image1.jpeg";
+import { StyleSheet } from "react-native";
+import RewardModal from "../modals/RewardModal";
+import ConfirmationModal from "../modals/ConfirmationModal";
+import receiveReward from "../../services/receiveReward";
+import { TEST_UID } from "../../api/constants";
 
-const OngoingChallengeCard = ({ ongoingChallenge, type, width }) => {
-  // console.log("OngoingChallengeCard", ongoingChallenge);
-  const [challengeType, setChallengeType] = useState("Daily");
-  const [progressRate, setProgressRate] = useState(null);
-  const [steps, setSteps] = useState(0);
-  const [distance, setDistance] = useState(0);
-  const [calories, setCalories] = useState(0);
-  const [timeLeft, setLeftTime] = useState("");
+const OngoingChallengeCard = ({ challenge, totalPageCount, currentPage }) => {
+  const [progressRate, setProgressRate] = useState(0);
+  const [showRewardButton, setShowRewardButton] = useState(false);
 
-  useEffect(() => {
-    setProgressRate(
-      (ongoingChallenge.activityData.steps / ongoingChallenge.daily_goal) * 100
-    );
-    setSteps(ongoingChallenge.activityData.steps);
-    setDistance(ongoingChallenge.activityData.distance);
-    setCalories(ongoingChallenge.activityData.calories);
-    setLeftTime(ongoingChallenge.timeLeft);
-  }, [ongoingChallenge]);
+  const [showRewardModal, setShowRewardModal] = useState(false);
+  const [showCancelModal, setShowCancelModal] = useState(false);
 
-  return (
-    <View style={[styles.card, { width: width }]}>
-      <Text style={styles.cardTitle}>{challengeType} Goal</Text>
-      <Text>Time left: {timeLeft}</Text>
-      <Image
-        source={image1}
-        style={styles.image}
-      />
-      <Text>Indicater with monster's name</Text>
-      <Center w="100%">
-        <Box
-          w="90%"
-          maxW="400"
-        >
-          <Progress
-            value={progressRate}
-            mx="4"
+  function handleCancel() {
+    setShowCancelModal(true);
+  }
+  function handleLeaveChallenge() {
+    // [TODO] Remove challenge progress from Database
+    console.log("handleLeaveChallenge pressed!!!!!!!!");
+  }
+
+  async function handleShowRewardModal(value) {
+    console.log("Pressed Reward", value);
+    setShowRewardModal(true);
+
+    // use TEST_UID for testing
+    await receiveReward(TEST_UID);
+  }
+
+  function handleCopyCode() {
+    console.log("Pressed copy code");
+  }
+
+  const renderCommonContent = () => {
+    return (
+      <>
+        <Text>{challenge.type} Goal</Text>
+        <Text>Time left: {challenge.remainingTime}</Text>
+        <Image
+          source={challenge.monsterImg}
+          alt="testImage"
+          size="lg"
+        />
+        <PagenationIndicator
+          totalPageCount={totalPageCount}
+          currentPage={currentPage}
+          monsterName={challenge.monsterName}
+        />
+        <Center w="100%">
+          <Box w="90%">
+            <Progress
+              colorScheme="primary"
+              value={progressRate}
+            />
+          </Box>
+        </Center>
+        {/* Switch button visibility depends on challenge status */}
+        <Box p={4}>
+          {showRewardButton && (
+            <Button onPress={handleShowRewardModal}>Receive Reward</Button>
+          )}
+          <RewardModal
+            showModal={showRewardModal}
+            setShowModal={setShowRewardModal}
+            size="xl"
+            reward={challenge.reward}
           />
         </Box>
-      </Center>
-      <Text>Distance {distance}</Text>
-      <Text>Steps {steps}</Text>
-      <Text>Calories {calories}</Text>
-    </View>
-  );
+        <Text>Current Progress</Text>
+        <HStack
+          space="1"
+          backgroundColor="amber.100"
+          padding="3"
+        >
+          <VStack>
+            <Text>{challenge.currentDistance}</Text>
+            <Text>KM</Text>
+          </VStack>
+          <Divider
+            orientation="vertical"
+            mx="3"
+            _light={{
+              bg: "muted.800",
+            }}
+            _dark={{
+              bg: "muted.50",
+            }}
+          />
+          <VStack>
+            <Text>{challenge.currentSteps}</Text>
+            <Text>Steps</Text>
+          </VStack>
+          <Divider
+            orientation="vertical"
+            mx="3"
+            _light={{
+              bg: "muted.800",
+            }}
+            _dark={{
+              bg: "muted.50",
+            }}
+          />
+          <VStack>
+            <Text>{challenge.currentCalories}</Text>
+            <Text>Kcal</Text>
+          </VStack>
+        </HStack>
+      </>
+    );
+  };
+
+  const renderDailyChallenge = () => {
+    useEffect(() => {
+      setProgressRate(
+        Math.floor((challenge.currentSteps / challenge.targetSteps) * 100)
+      );
+      if (challenge.finishChallenge && !challenge.getReward) {
+        setShowRewardButton(true);
+      }
+    }, [challenge.currentSteps, challenge.targetSteps]);
+    return (
+      <View style={[styles.card, showCancelModal ? styles.blur : null]}>
+        {renderCommonContent()}
+      </View>
+    );
+  };
+
+  const renderEventChallenge = () => {
+    useEffect(() => {}, []);
+
+    return (
+      <View style={[styles.card, showCancelModal ? styles.blur : null]}>
+        {renderCommonContent()}
+        <Text>badge info: empty now{challenge.badgeInfo}</Text>
+        <Button onPress={handleCancel}>Cancel</Button>
+        <ConfirmationModal
+          showModal={showCancelModal}
+          setShowModal={setShowCancelModal}
+          size="xl"
+          onSubmit={handleLeaveChallenge}
+          submitBtnLabel="Leave event"
+        >
+          Are you sure you want to leave?
+        </ConfirmationModal>
+      </View>
+    );
+  };
+
+  const renderGroupChallenge = () => {
+    return (
+      <View style={[styles.card, showCancelModal ? styles.blur : null]}>
+        {renderCommonContent()}
+        <Text>Avator icons: test, test, test</Text>
+        <Text>code: testtest</Text>
+        <Button
+          title="Copy"
+          onPress={handleCopyCode}
+        />
+        <Text>badge info: empty now{challenge.badgeInfo}</Text>
+        <Button onPress={handleCancel}>Cancel</Button>
+        <CancelModal
+          showModal={showModal}
+          setShowModal={setShowModal}
+          handleCancel={handleCancel}
+        />
+      </View>
+    );
+  };
+
+  switch (challenge.type) {
+    case "daily":
+      return renderDailyChallenge();
+
+    case "event":
+      return renderEventChallenge();
+
+    case "group":
+      return renderGroupChallenge();
+
+    default:
+      return <Text>Error... something's going on TT</Text>;
+  }
 };
 
 export default OngoingChallengeCard;
 
+function PagenationIndicator({ totalPageCount, currentPage, monsterName }) {
+  return (
+    <View style={styles.indicatorContainer}>
+      {Array.from({ length: totalPageCount }, (_, index) => {
+        const pageNumber = index + 1;
+        const isCurrentPage = pageNumber === currentPage;
+
+        return (
+          <View
+            key={index}
+            style={[
+              styles.indicator,
+              isCurrentPage ? styles.selectedIndicator : null,
+            ]}
+          >
+            {isCurrentPage ? <Text>{monsterName}</Text> : null}
+          </View>
+        );
+      })}
+    </View>
+  );
+}
+
 const styles = StyleSheet.create({
   card: {
     flex: 1,
+    padding: 20,
     justifyContent: "center",
     alignItems: "center",
-    backgroundColor: "lightgray",
+    backgroundColor: "aliceblue",
+  },
+  blur: {
+    opacity: 0.5,
   },
   cardTitle: {
     fontSize: 24,
@@ -65,5 +238,22 @@ const styles = StyleSheet.create({
   image: {
     width: 100,
     height: 100,
+  },
+  indicatorContainer: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  indicator: {
+    width: 10,
+    height: 10,
+    borderRadius: 5,
+    margin: 5,
+    backgroundColor: "lightgray",
+  },
+  selectedIndicator: {
+    width: 72,
+    height: 40,
+    backgroundColor: "grey",
   },
 });
