ee4f1088e699f218227225dac874a8cdb3171e27
Rena Sakata <rena.sakata.cn@gmail.com>
2023-07-06 22:43:45 -0700

    Error handling

--- src/services/saveActivityData.js
+++ src/services/saveActivityData.js
@@ -32,9 +32,18 @@ async function saveActivityData(user_id) {
     if (isExistingRecord) {
       // Calculated the difference of steps here
       // and apply it to event & team challenge
-      const differenceOfSteps = activityData.steps - todayRecord.steps;
-      const differenceOfDistance = activityData.distance - todayRecord.distance;
-      const differenceOfCalories = activityData.calories - todayRecord.calories;
+      const differenceOfSteps =
+        activityData.steps - todayRecord.steps > 0
+          ? activityData.steps - todayRecord.steps
+          : 0;
+      const differenceOfDistance =
+        activityData.distance - todayRecord.distance > 0
+          ? activityData.distance - todayRecord.distance
+          : 0;
+      const differenceOfCalories =
+        activityData.calories - todayRecord.calories > 0
+          ? activityData.calories - todayRecord.calories
+          : 0;
 
       // Update Event and Group Challenge
       const responseUpdatedChallengeProgresses =
@@ -141,6 +150,7 @@ const updateEventAndGroupChallengeProgresses = async (
             current_distance:
               challengeProgressData.current_distance + differenceOfDistance,
           };
+          // Math.floor(challengeProgressInfo.data.current_calories * 100) / 100;
 
           await updateChallengeProgress(
             challengeProgress_id,
