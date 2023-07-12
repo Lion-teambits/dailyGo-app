329506286f5813aa51cb9b5cb923223d386c3913
Rena Sakata <rena.sakata.cn@gmail.com>
2023-07-12 11:09:21 -0700

    Remove console.log

--- src/screens/tabnav/HomeScreen.js
+++ src/screens/tabnav/HomeScreen.js
@@ -56,7 +56,6 @@ const HomeScreen = ({ route }) => {
       setOngoingChallenges(newOngoingChallenges);
       const responseUserInfo = await retrieveUserInfo(user_id);
       setUserInfo(responseUserInfo);
-      console.log("HOME", responseUserInfo);
 
       setIsLoading(false);
     }
