705db336aa46d1b3d0cc2863b7eac3827353b195
Jungho Lee <nightfallb612@gmail.com>
2023-07-12 00:22:52 -0700

    Challenge list update whenever focused.

--- src/screens/tabnav/ChallengesScreen.js
+++ src/screens/tabnav/ChallengesScreen.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { View } from "react-native";
 import ChallengeList from "../../components/list/ChallengeList";
 import { ScrollView } from "native-base";
@@ -6,18 +6,22 @@ import GroupEventCard from "../../components/cards/GroupEventCard";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { retrieveUserInfo } from "../../api/userService";
 import GroupChallengeList from "../../components/list/GroupChallengeList";
+import { useFocusEffect } from "@react-navigation/native";
 
 const ChallengesScreen = () => {
   const [userData, setUserData] = useState("");
 
-  useEffect(() => {
-    const getUserInfo = async () => {
-      const uid = await AsyncStorage.getItem("@uid");
-      const userInfo = await retrieveUserInfo(uid);
-      setUserData(userInfo);
-    };
-    getUserInfo();
-  }, []);
+  useFocusEffect(
+    useCallback(() => {
+      const getUserInfo = async () => {
+        const uid = await AsyncStorage.getItem("@uid");
+        const userInfo = await retrieveUserInfo(uid);
+        setUserData(userInfo);
+        console.log("[ChallengesScreen]userInfo: ", userInfo);
+      };
+      getUserInfo();
+    }, [])
+  );
 
   return (
     <ScrollView>
