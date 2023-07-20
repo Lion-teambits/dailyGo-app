0759ccc2cbfc72a5568f1681f568ffc436b417b1
dkiatsongsak00 <dkiatsongsak00@mylangara.ca>
2023-07-20 00:37:02 -0700

    Apply styling to preferences and onboarding screen

--- src/components/listitems/CarouselItem.js
+++ src/components/listitems/CarouselItem.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { View, Text, Image, Dimensions } from "react-native";
+import { TXT_DARK_BG } from "../../constants/colorCodes";
 
 const windowWidth = Dimensions.get("window").width;
 
@@ -8,6 +9,7 @@ const CarouselItem = ({ item }) => {
     <View style={styles.carouselItem}>
       <Image source={item.image} style={styles.image} />
       <Text style={styles.description}>{item.description}</Text>
+      <Text style={styles.description2}>{item.description2}</Text>
     </View>
   );
 };
@@ -17,17 +19,28 @@ const styles = {
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    width: windowWidth - 120,
+    width: windowWidth,
+    marginTop: "50%",
   },
   image: {
-    width: 200,
-    height: 200,
+    width: 360,
+    height: 243,
+    resizeMode: "contain",
     marginBottom: 10,
   },
   description: {
-    fontSize: 16,
+    fontSize: 18,
     fontWeight: "bold",
+    color: TXT_DARK_BG,
     textAlign: "center",
+    padding: "10%",
+  },
+  description2: {
+    fontSize: 14,
+    textAlign: "center",
+    color: TXT_DARK_BG,
+    paddingLeft: "10%",
+    paddingRight: "10%",
   },
 };
 
