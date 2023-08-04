65dd6c7c3df5316d8075a2763556248e225d8dd8
Rena Sakata <rena.sakata.cn@gmail.com>
2023-08-03 22:47:10 -0700

    Add Splash screen animation

--- App.js
+++ App.js
@@ -1,11 +1,17 @@
-import React, { useEffect } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { NativeBaseProvider } from "native-base";
 import AppStack from "./src/stack/AppStack";
 import startBackgroundTask from "./src/services/startBackgroundTask";
-import { LogBox } from "react-native";
+import { LogBox, StyleSheet, View } from "react-native";
 import { BUILD_MODE } from "./src/constants/buildOptions";
+import * as SplashScreen from "expo-splash-screen";
+import LottieView from "lottie-react-native";
+
+SplashScreen.preventAutoHideAsync();
 
 const App = () => {
+  const [appIsReady, setAppIsReady] = useState(false);
+
   useEffect(() => {
     // start background task after mounting
     startBackgroundTask();
@@ -14,7 +20,40 @@ const App = () => {
       LogBox.ignoreLogs(["Warning: ..."]); // Ignore log notification by message
       LogBox.ignoreAllLogs(); //Ignore all log notifications
     }
+
+    async function prepare() {
+      try {
+        await new Promise((resolve) => setTimeout(resolve, 3000));
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        setAppIsReady(true);
+      }
+    }
+
+    prepare();
   }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (appIsReady) {
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
+
+  if (!appIsReady) {
+    return (
+      <View
+        style={styles.splashContainer}
+        onLayout={onLayoutRootView}
+      >
+        <LottieView
+          source={require("./assets/images/Splashscreen_DailyGo.json")}
+          autoPlay
+          loop={false}
+        />
+      </View>
+    );
+  }
 
   return (
     <NativeBaseProvider>
@@ -24,3 +63,17 @@ const App = () => {
 };
 
 export default App;
+
+const styles = StyleSheet.create({
+  splashContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "white",
+  },
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
